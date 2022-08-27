3ff25be54b43488b52fcaa5dde4ca999b6df68c1
Nate Marks <nmarks@imprivata.com>
2022-08-27 07:23:14 -0400

    json logging

--- cmd/puppers/main.go
+++ cmd/puppers/main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"os"
 
 	"github.com/natemarks/puppers/version"
+	"github.com/rs/zerolog"
 )
 
+func getLogger() (logMe *zerolog.Logger) {
+	logFile, err := os.OpenFile("puppers.log",
+		os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		panic(err)
+	}
+	logger := zerolog.New(logFile).With().Timestamp().Logger()
+	logger = logger.With().Str("version", version.Version).Logger()
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	return &logger
+}
+
 func main() {
-	fmt.Printf("version: %s\n", version.Version)
+	logger := getLogger()
+	logger.Info().Msg("Puppers Starting")
+	logger.Info().Msg("Puppers Complete")
 }
