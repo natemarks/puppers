a98ff876ff593d9e2916810cbb8870c258fc7c48
Nate Marks <nmarks@imprivata.com>
2022-08-28 05:45:28 -0400

    Stop writing to a local log file and log to cloudwatch instead

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+### v0.0.1
+log to a local file
+
+### v0.0.2
+log to cloudwatch logs
--- cmd/puppers/main.go
+++ cmd/puppers/main.go
@@ -1,33 +1,153 @@
 package main
 
+// Package main runs on a loop creating JSON log messages and pushing them to cloudwatch
+// I stole the logic entirely from https://github.com/mathisve/golang-cloudwatch-logs-example
 import (
-	"fmt"
-	"os"
+	"log"
+	"sync"
 	"time"
 
-	"github.com/natemarks/puppers/version"
-	"github.com/rs/zerolog"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
+	"github.com/google/uuid"
+	"github.com/natemarks/puppers/datasource"
 )
 
-func getLogger() (logMe *zerolog.Logger) {
-	logFile, err := os.OpenFile("puppers.log",
-		os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+var (
+	cwl           *cloudwatchlogs.CloudWatchLogs
+	logGroupName  = "puppers"
+	logStreamName = ""
+	sequenceToken = ""
+)
+
+func init() {
+	sess, err := session.NewSessionWithOptions(session.Options{
+		Config: aws.Config{
+			Region: aws.String("us-east-1"),
+		},
+	})
+
 	if err != nil {
 		panic(err)
 	}
-	logger := zerolog.New(logFile).With().Timestamp().Logger()
-	logger = logger.With().Str("version", version.Version).Logger()
-	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-	zerolog.SetGlobalLevel(zerolog.InfoLevel)
-	return &logger
+
+	cwl = cloudwatchlogs.New(sess)
+
+	err = ensureLogGroupExists(logGroupName)
+	if err != nil {
+		panic(err)
+	}
+
 }
 
 func main() {
-	logger := getLogger()
-	logger.Info().Msg("Puppers Starting")
-	fmt.Println("Puppers starting. Ctrl-C to interrupt")
-	for {
-		logger.Info().Msg("I'm alive!!")
-		time.Sleep(4 * time.Second)
+	queue := []string{}
+	lock := sync.Mutex{}
+
+	go datasource.GenerateData(&queue, &lock)
+
+	go processQueue(&queue, &lock)
+
+	// to stop the code from exiting
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	wg.Wait()
+
+}
+
+// ensureLogGroupExists first checks if the log group exists,
+// if it doesn't it will create one.
+func ensureLogGroupExists(name string) error {
+	resp, err := cwl.DescribeLogGroups(&cloudwatchlogs.DescribeLogGroupsInput{LogGroupNamePrefix: &name})
+	if err != nil {
+		return err
 	}
+
+	for _, logGroup := range resp.LogGroups {
+		if *logGroup.LogGroupName == name {
+			return nil
+		}
+	}
+
+	_, err = cwl.CreateLogGroup(&cloudwatchlogs.CreateLogGroupInput{
+		LogGroupName: &name,
+	})
+	if err != nil {
+		return err
+	}
+
+	_, err = cwl.PutRetentionPolicy(&cloudwatchlogs.PutRetentionPolicyInput{
+		RetentionInDays: aws.Int64(14),
+		LogGroupName:    &name,
+	})
+
+	return err
+}
+
+// createLogStream will make a new logStream with a random uuid as its name.
+func createLogStream() error {
+	name := uuid.New().String()
+
+	_, err := cwl.CreateLogStream(&cloudwatchlogs.CreateLogStreamInput{
+		LogGroupName:  &logGroupName,
+		LogStreamName: &name,
+	})
+
+	logStreamName = name
+
+	return err
+}
+
+// processQueue will process the log queue
+func processQueue(queue *[]string, lock *sync.Mutex) error {
+	var logQueue []*cloudwatchlogs.InputLogEvent
+
+	for {
+		lock.Lock()
+		if len(*queue) > 0 {
+			for _, item := range *queue {
+				logQueue = append(logQueue, &cloudwatchlogs.InputLogEvent{
+					Message:   &item,
+					Timestamp: aws.Int64(time.Now().UnixNano() / int64(time.Millisecond)),
+				})
+			}
+
+			*queue = []string{}
+		}
+
+		lock.Unlock()
+
+		if len(logQueue) > 0 {
+			input := cloudwatchlogs.PutLogEventsInput{
+				LogEvents:    logQueue,
+				LogGroupName: &logGroupName,
+			}
+
+			if sequenceToken == "" {
+				err := createLogStream()
+				if err != nil {
+					panic(err)
+				}
+			} else {
+				input = *input.SetSequenceToken(sequenceToken)
+			}
+
+			input = *input.SetLogStreamName(logStreamName)
+
+			resp, err := cwl.PutLogEvents(&input)
+			if err != nil {
+				log.Println(err)
+			}
+
+			if resp != nil {
+				sequenceToken = *resp.NextSequenceToken
+			}
+
+			logQueue = []*cloudwatchlogs.InputLogEvent{}
+		}
+
+		time.Sleep(time.Second * 5)
+	}
+
 }
