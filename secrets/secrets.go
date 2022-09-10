348040a8e84bdbbe4e0f61d18b77bdb7b2dad974
Nate Marks <nmarks@imprivata.com>
2022-09-10 06:43:34 -0400

    Add a new pupperswebserver executable

--- secrets/secrets.go
+++ secrets/secrets.go
@@ -3,6 +3,8 @@ package secrets
 import (
 	"context"
 	"encoding/json"
+	"errors"
+	"os"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -36,5 +38,16 @@ func GetRDSCredentials(secretName string, log *zerolog.Logger) (connectionParams
 	if err != nil {
 		log.Panic().Msg(err.Error())
 	}
+	log.Info().Msgf("found credentials for hostname: %s", connectionParams.Host)
 	return connectionParams
+}
+
+// GetSecretFromEnvar get the RDS secret name from PUPPERS_SECRET_NAME env var
+func GetSecretFromEnvar() string {
+	// secretName "SecretA720EF05-2pmGVjf2abKX"
+	secretName, set := os.LookupEnv("PUPPERS_SECRET_NAME")
+	if !set {
+		panic(errors.New("PUPPERS_SECRET_NAME is not set"))
+	}
+	return secretName
 }
